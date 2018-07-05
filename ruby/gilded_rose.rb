dbc051019cdfba208ad33c1f409191d24504251d
Shehbaz Ahmad <shehbazbashir@gmail.com>
2018-07-05 11:59:46 +0100

    Move sell in and quality change logic to separate class
    Create a class to handle change for normal cases

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -3,52 +3,9 @@ class GildedRose
   def initialize(items)
     @items = items
   end
-
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      DefaultUpdater.new(item).update               
     end
   end
 end
@@ -64,5 +21,30 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class DefaultUpdater
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    item.sell_in = item.sell_in - 1
+    change_quality
+  end
+  
+  def change_quality
+    if item.sell_in > 0
+      quality = item.quality - 1
+    else
+      quality = item.quality - 2
+    end
+
+    if quality >= 0
+      item.quality = quality 
+    end
   end
 end
